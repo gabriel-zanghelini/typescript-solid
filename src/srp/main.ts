970876a69cf8a3398d97076623b80784ebd1f0d7
Gabriel Zanghelini <zanghelinigab@gmail.com>
2023-03-08 20:48:29 -0300

    folder organization

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -1,6 +1,6 @@
-import { Messaging } from './messaging';
+import { Messaging } from './services/messaging';
 import { Order } from './entities/order';
-import { Persistency } from './persistency';
+import { Persistency } from './services/persistency';
 import { ShoppingCart } from './entities/shopping-cart';
 import { Product } from './entities/product';
 
@@ -9,8 +9,8 @@ const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
 
-shoppingCart.addItem(new Product('Camiseta', 49.9));
-shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
 shoppingCart.addItem(new Product('Lápis', 1.59));
 
 console.log(shoppingCart.items);
