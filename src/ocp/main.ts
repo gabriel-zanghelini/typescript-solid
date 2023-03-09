9e4d7ff60e7ef29ed3aac6a548413b0c7f4ec033
Gabriel Zanghelini <zanghelinigab@gmail.com>
2023-03-08 22:07:56 -0300

    add discount with OCP

---
+++ src/ocp/main.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Product } from './classes/product';
+import {
+  FiftyPercentDiscount,
+  NoDiscount,
+  TenPercentDiscount,
+} from './classes/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+const noDiscount = new NoDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.91));
+shoppingCart.addItem(new Product('Caderno', 9.9123));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
